d04c048387931d32e2911e08d43bd3855324f636
Yichao Zhou <broken.zhou@gmail.com>
2018-02-13 12:50:13 -0800

    Update post solver

--- src/parametrizer.cpp
+++ src/parametrizer.cpp
@@ -11,8 +11,8 @@
 #include <Eigen/Sparse>
 #include <fstream>
 #include <list>
-#include <queue>
 #include <map>
+#include <queue>
 #include <set>
 
 void Parametrizer::ComputeIndexMap(int with_scale) {
@@ -59,26 +59,25 @@ void Parametrizer::ComputeIndexMap(int with_scale) {
 #endif
     int t1 = GetCurrentTime64();
     FixFlipHierarchy();
-    
+
     int t2 = GetCurrentTime64();
     printf("Flip use %lf\n", (t2 - t1) * 1e-3);
 
     subdivide_diff(F, V, N, Q, O, V2E, hierarchy.mE2E, boundary, nonManifold, edge_diff,
                    edge_values, face_edgeOrients, face_edgeIds, singularities, 1);
-    
 
     Optimizer::optimize_positions_fixed(hierarchy, edge_values, edge_diff, with_scale);
-    
+
     ExtractQuadMesh();
-    
+
 #ifdef LOG_OUTPUT
     printf("Fix holes...\n");
 #endif
     compute_direct_graph_quad(O_compact, F_compact, V2E_compact, E2E_compact, boundary_compact,
                               nonManifold_compact);
-    
-//    FixHoles();
-    
+
+    FixHoles();
+
     compute_direct_graph_quad(O_compact, F_compact, V2E_compact, E2E_compact, boundary_compact,
                               nonManifold_compact);
     // potential bug, not guarantee to have quads at holes!
@@ -88,9 +87,7 @@ void Parametrizer::ComputeIndexMap(int with_scale) {
 #ifdef LOG_OUTPUT
     printf("Optimize quad positions...\n");
 #endif
-    
-    optimize_quad_positions(O_compact, N_compact, Q_compact, F_compact, V2E_compact, E2E_compact,
-                            V, N, Q, O, F, V2E, hierarchy.mE2E, disajoint_tree, hierarchy.mScale, true);
-    
-}
 
+    optimize_quad_positions(O_compact, N_compact, Q_compact, F_compact, V2E_compact, E2E_compact,
+                            V, N, Q, O, F, V2E, hierarchy.mE2E, disajoint_tree, hierarchy.mScale);
+}
